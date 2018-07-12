176211eb185a19778a74d864fff83993ae81030a
Walter Scarborough <walter.scarborough@gmail.com>
2018-07-11 20:24:00 -0400

    Expanded test cases to handle error paths

--- test/index.test.js
+++ test/index.test.js
@@ -8,4 +8,30 @@ describe('spring-cloud-contract-json-reader', () => {
 
     expect(contract).toEqual('[{"name": "sombrero", "color": "plaid"}, {"name": "baseball cap", "color": "zebra stripe"}, {"name": "tophat", "color": "polka-dot"}]');
   });
+
+  it('should fail a promise if it can not find contract test data', async () => {
+    expect.assertions(1);
+    process.env.CONTRACT_JSON_BASE_PATH = 'test/fixtures';
+
+    const readJsonContractFile = require('../src/index');
+
+    try {
+      await readJsonContractFile('some-nonexistent-file.json');
+    } catch (e) {
+      expect(e).not.toBeNull();
+    }
+  });
+
+  it('should fail a promise if env var "CONTRACT_JSON_BASE_PATH" is not set', async () => {
+    expect.assertions(1);
+    process.env.CONTRACT_JSON_BASE_PATH = undefined;
+
+    const readJsonContractFile = require('../src/index');
+
+    try {
+      await readJsonContractFile('sample-contract.json');
+    } catch (e) {
+      expect(e).not.toBeNull();
+    }
+  });
 });
